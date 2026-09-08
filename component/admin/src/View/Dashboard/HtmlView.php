953047c9b0bfc19b9a17089b405014d39f4fab26
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 23:03:43 +0200

    Use optional Core shared UI assets on Tasks dashboard

--- component/admin/src/View/Dashboard/HtmlView.php
+++ component/admin/src/View/Dashboard/HtmlView.php
@@ -4,11 +4,16 @@ namespace Xdecaro\Component\Decarotasks\Administrator\View\Dashboard;
 defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Xdecaro\Core\Asset\AssetService;
 
 final class HtmlView extends BaseHtmlView
 {
     public function display($tpl = null): void
     {
+        if (class_exists(AssetService::class)) {
+            (new AssetService())->useComponents($this->getDocument()->getWebAssetManager());
+        }
+
         parent::display($tpl);
     }
 }
